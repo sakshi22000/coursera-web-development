295c6c00e521fb747d1c357df12491f375b4712c
sakshi <sakshigujral04@gmail.com>
2021-01-18 12:58:52 +0530

    Module4

--- module4/script.js
+++ module4/script.js
@@ -5,18 +5,18 @@ Expected output:
 
 Hello Yaakov
 Good Bye John
-Good Bye Jayant
-Good Bye Sakshi
+Good Bye Jen
+Good Bye Jason
 Hello Paul
-Hello Jason
-Hello Neetu
-Hello Manish
-Hello Bharat
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
 Good Bye Jim
 */
 
 (function() {
-    var names = ["Yaakov", "John", "Jayant", "Sakshi", "Paul", "Jason", "Neetu", "Manish", "Bharat", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
